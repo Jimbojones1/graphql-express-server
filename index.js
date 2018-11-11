bee1bd4f53c148ff9505173cfabe94594737dd90
Jim Haff <Jimmyhaff3@gmail.com>
2018-11-11 23:57:56 +0000

    graphql playground setup

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-import express from 'express';
+import express     from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema      from './schema';
+
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('app is working')
 });
 
+
+const roote = {hello: () => 'Hi Im larry'};
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: roote,
+  graphiql: true
+}));
 
 app.listen(3000, () => {
   console.log('server is running')
