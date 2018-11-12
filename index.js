e405a5e60dd46cbdba4085cbf60f94e9f8f0dbbe
Jim Haff <Jimmyhaff3@gmail.com>
2018-11-12 16:17:42 +0000

    added getFriend and seperated into resolvers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express     from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema      from './schema';
-
+import resolvers   from './resolvers';
 
 const app = express();
 
@@ -9,42 +9,12 @@ app.get('/', (req, res) => {
   res.send('app is working')
 });
 
-class Friend {
-  constructor(id, { firstName, lastName, gender, email, language}){
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.email = email;
-    this.language = language;
-  }
-}
 
-const friendDatabase = {};
-
-const roote = {
-  friend: () => {
-    return {
-      "id": 28143,
-      "firstName": 'Jim',
-      "lastName": 'Haff',
-      "gender": 'Male',
-      "language": 'AMERICAN',
-      "email": 'woah.com'
-    }
-  },
-  createFriend: ({input}) => {
-    const id = require('crypto').randomBytes(10).toString('hex');
-    friendDatabase[id] = input;
-    return new Friend(id, input);
-  }
-
-};
 
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: roote,
+  rootValue: resolvers,
   graphiql: true
 }));
 
